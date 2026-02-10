fce31ec39f2f9843d06baac7a038266f17e501a9
MIMI-time-dev <kusakabe919@gmail.com>
2026-02-10 17:26:15 +0900

    Add files via upload

--- MIMI_Time_main.py
+++ MIMI_Time_main.py
@@ -427,6 +427,7 @@ def about():
     return render_template_string(ABOUT_HTML)
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
 
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
